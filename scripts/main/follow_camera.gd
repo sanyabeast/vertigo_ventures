6155223d010d55f7eb04b740636b05ba88b1a99c
sanyabeast <a.gvrnsk@gmail.com>
2023-10-04 00:07:39 +0300

    tweaks and experiments

--- scripts/main/follow_camera.gd
+++ scripts/main/follow_camera.gd
@@ -44,24 +44,24 @@ func _process(delta):
 		var desired_position = Vector3(target.global_transform.origin.x, global_transform.origin.y, target.global_transform.origin.z)
 		
 		# Check if the target is of type VCharacter
-		if target is VCharacter:
-			# Get the target's move direction
-			var direction: Vector3 = target.move_direction
-			
-			# Update the position of the camera_root based on the target's move direction
-			camera_root.position = camera_root.position.move_toward(Vector3(
-				# Lerp between the left and right offsets based on the target's X direction
-				lerp(camera_direction_shift.x, -camera_direction_shift.x, (direction.x + 1) / 2),
-				
-				# Lerp the Y offset based on the target's move direction magnitude
-				lerp(0.0, camera_direction_shift.y, direction.length()),
-				
-				# Lerp the Z offset based on the target's forward/backward direction
-				lerp(0.0, camera_direction_shift.z, abs(direction.z)),
-			), camera_direction_shift_ease_speed * delta)
+#		if target is VCharacter:
+#			# Get the target's move direction
+#			var direction: Vector3 = target.move_direction
+#
+#			# Update the position of the camera_root based on the target's move direction
+#			camera_root.position = camera_root.position.move_toward(Vector3(
+#				# Lerp between the left and right offsets based on the target's X direction
+#				lerp(camera_direction_shift.x, -camera_direction_shift.x, (direction.x + 1) / 2),
+#
+#				# Lerp the Y offset based on the target's move direction magnitude
+#				lerp(0.0, camera_direction_shift.y, direction.length()),
+#
+#				# Lerp the Z offset based on the target's forward/backward direction
+#				lerp(0.0, camera_direction_shift.z, abs(direction.z)),
+#			), camera_direction_shift_ease_speed * delta)
 		
 		# Set the camera root's global position to the previously calculated desired position
 		global_transform.origin = desired_position
 		
 		# Make the camera look at the target's global position
-		camera.look_at(target.global_position);
+#		camera.look_at(target.global_position + Vector3(0, -0.25, 0));
